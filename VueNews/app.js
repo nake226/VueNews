5b9fc39168891e9e48685c5dc812acb58e710116
nakai <takayuki.nakai@nijibox.co.jp>
2018-04-12 13:27:13 +0900

    change: refactoring source -> use url as add: methods -> to connect NYT and get posts change: mounted -> just refer methods(getPosts)

--- VueNews/app.js
+++ VueNews/app.js
@@ -1,16 +1,39 @@
 'use strict';
 
+// NYT-API base url
+const ApiBaseUrl = 'https://api.nytimes.com/svc/topstories/v2/';
+// NYT-API Key
+const ApiKey = 'd6c24974592e4c28a27e2c7eecc4c5fe';
+
+/**
+ * @returns url which axios connects.
+ * @param {*} url 
+ */
+function setUrl(url){
+  return ApiBaseUrl + url + '.json?api-key=' + ApiKey;
+}
+
 const vm = new Vue({
   el: '#app',
   data: {
-    results: [
-      // news contents area
-    ]
-  },
+    results: [ /* news posts data */ ]
+  }, 
   mounted(){
-    axios.get("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=d6c24974592e4c28a27e2c7eecc4c5fe")
-    .then(response => {
-      this.results = response.data.results
-    })
+    this.getPosts('home');
+  },
+  methods: {
+    getPosts(section){
+      let url = setUrl(section);
+      axios.get(url)
+      .then(
+        response => {
+          this.results = response.data.results;
+        }
+      ).catch(
+        error => {
+          console.log(error);
+        }
+      );
+    }
   }
 });
